b5a14b2624f8e6e9b207a29aed5c714a1d0d4593
Maria Jankowiak <jankowiakmaria@gmail.com>
2015-02-28 23:26:02 +0000

    Added dates setting

--- app.js
+++ app.js
@@ -12,6 +12,19 @@ var sendKeys = function(page, selector, keys){
     page.sendEvent("keypress", keys);
 };
 
+var replaceCurrentContent = function(page, selector, keys){
+  page.evaluate(function(selector){
+      // focus on the text element before typing
+      var element = document.querySelector(selector);
+      element.click();
+      element.focus();
+  }, selector);
+
+  page.sendEvent("keypress", page.event.key.A, null, null, 0x04000000);
+  page.sendEvent("keypress", page.event.key.Delete);
+  page.sendEvent("keypress", keys);
+};
+
 var isReady = false;
 
 var flightPage = require('webpage').create();
@@ -20,11 +33,20 @@ flightPage.onConsoleMessage = function(msg) {
     console.log('console: ' + msg);
 };
 
+
+//todo: parameters
 flightPage.open(config.ryanair.page, function (status) {
   if(status === "success") {
     sendKeys(flightPage, ".stations select[title='Origin']", "London (Stansted)");
     sendKeys(flightPage, ".stations select[title='Destination']", "Pozna");
 
+    replaceCurrentContent(flightPage, "[name='SearchInput$DeptDate']", "08/03/2015");
+    replaceCurrentContent(flightPage, "[name='SearchInput$RetDate']", "08/04/2015");
+//$("#SearchInput_RoundTrip").click();
+//$("#SearchInput_OneWay").click();
+
+
+    flightPage.render("replacedDates.png");
     //todo: add the rest of parameters
 
     flightPage.evaluate(function() {
@@ -49,12 +71,12 @@ flightPage.open(config.ryanair.page, function (status) {
       console.log(JSON.stringify(result, undefined, 4));
       phantom.exit();
     }, 5000);
+    //phantom.exit();
   }
 });
 
 flightPage.onResourceReceived = function (response) {
   if(flightPage.url === "https://www.bookryanair.com/SkySales/Booking.aspx#Select"){
     isReady = true;
-    //console.log('Receive', flightPage.url, JSON.stringify(response, undefined, 4));
   }
 };
