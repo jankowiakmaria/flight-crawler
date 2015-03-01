9ee9cb417cf3c515e58d468e5fd84b55528b554a
Maria Jankowiak <jankowiakmaria@gmail.com>
2015-03-01 23:02:18 +0000

    partially working

--- app.js
+++ app.js
@@ -14,41 +14,46 @@ var crawl = function(error, parameters){
   }
 
   var providerConfig = config[parameters.provider];
-  var currentDay = parameters.startDate;
-  var flightPage = webPage.create();
+  var flightsLeft = 0;
 
-  function proc(providerConfig, parameters, currentDay) {
-    processOneDay(flightPage, providerConfig, parameters, currentDay.format(providerConfig.dateFormat), function(error, data){
+  function proc(providerConfig, parameters, currentDay, origin, destination) {
+    flightsLeft++;
+    processOneDay(providerConfig, parameters, currentDay.format(providerConfig.dateFormat), origin, destination, function(error, data){
       if(error){
-        console.log("----",'error', error);
+        console.log('error', error);
       }
       else{
-        console.log("----",JSON.stringify(data, undefined, 4));
+        console.log(JSON.stringify(data, undefined, 4));
       }
+      flightsLeft--;
       currentDay.add(1, "days");
       if(currentDay <= parameters.endDate){
-        console.log("----", "next day", currentDay.format(providerConfig.dateFormat))
-        setTimeout(function(){
-          proc(providerConfig, parameters, currentDay);
-        }, 1000);
+        proc(providerConfig, parameters, currentDay, origin, destination);
       }else{
-        phantom.exit();
+        if(flightsLeft === 0){
+          console.log("end");
+          phantom.exit();
+        }
       }
     });
   };
 
-  proc(providerConfig, parameters, currentDay);
+  proc(providerConfig, parameters, moment(parameters.startDate), parameters.origin, parameters.destination);
+  proc(providerConfig, parameters, moment(parameters.startDate), parameters.destination, parameters.origin);
 };
 
-var processOneDay = function(flightPage, providerConfig, parameters, currentDay, callback){
+//for ryanair
+var processOneDay = function(providerConfig, parameters, currentDay, origin, destination, callback){
+  var flightPage = webPage.create();
   var loadTimeout;
   var maxTimeout = 5000;
-  var t;
+  var done = false;
 
   flightPage.onResourceReceived = function (response) {
-    if(flightPage.url === providerConfig.resultPage){
+    if(flightPage.url === providerConfig.resultPage && !done){
+      done = true;
       clearTimeout(loadTimeout);
-      getResults(flightPage, t);
+      getResults(flightPage);
     }
   };
 
@@ -57,57 +62,45 @@ var processOneDay = function(flightPage, providerConfig, parameters, currentDay,
   };
 
 //todo: what if more than one flight in a day
-  var getResults = function(flightPage, t) {
-    console.log(currentDay, "afterTimeout", Date.now() - t);
-    var result = flightPage.evaluate(function(){
+  var getResults = function(flightPage) {
+    var result = flightPage.evaluate(function(currentDay){
       var result = [];
       $("article.selectFlights:not(#businessPlusBannerOffer)").each(function(index, element){
         var flight = $(element).find("a.active");
         result.push({
           flight: $(element).children("h1").text(),
-          date: $(element).find("caption").text().trim(),
+          date: currentDay,//$(element).find("caption").text().trim(),
           price: flight.find("div:not(.ng-hide)").text(),
           currency: flight.children("span").text()
         });
       });
       return result;
-    });
+    }, currentDay);
 
-    flightPage.close(function(){
-      console.log("closed?!");
-    });
-    console.log(currentDay, JSON.stringify(result, undefined, 4));
+    flightPage.close();
     callback(null, result);
   };
 
   flightPage.open(providerConfig.searchPage, function (status) {
     if(status === "success") {
-      console.log(currentDay, 0);//.format(providerConfig.dateFormat));
       setTimeout(function(){  //temporary fix
       //todo: wait until loaded!!!!
-  console.log(currentDay, 1);
-        ph.set(flightPage, ".stations select[title='Origin']", parameters.origin);
-        ph.set(flightPage, ".stations select[title='Destination']", parameters.destination);
-        ph.replace(flightPage, "[name='SearchInput$DeptDate']", currentDay);//.format(providerConfig.dateFormat));
-        //ph.replace(flightPage, "[name='SearchInput$RetDate']", parameters.endDate.format(providerConfig.dateFormat));
-    //$("#SearchInput_RoundTrip").click();
-    //$("#SearchInput_OneWay").click();
-  console.log(currentDay, 2);
-        //todo: add the rest of parameters
+        ph.set(flightPage, ".stations select[title='Origin']", origin);
+        ph.set(flightPage, ".stations select[title='Destination']", destination);
+        ph.replace(flightPage, "[name='SearchInput$DeptDate']", currentDay);
 
         flightPage.evaluate(function() {
           $("#SearchInput_OneWay").click();
           $("#SearchInput_ButtonSubmit").click(); //todo: also wait
         });
-        t = Date.now();
-  console.log(currentDay, 3);
+
         loadTimeout = setTimeout(function(){  //wait until page is loaded
-          getResults(flightPage, t);
+          getResults(flightPage);
         }, maxTimeout);
-      }, 0);
+      }, 1000);
     }
     else{
-      //flightPage.close();
+      flightPage.close();
       callback('Unable to load page!', null);
     }
   });
