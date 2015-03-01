3d015b21e1376848827689baa6f8b8b26c32b053
Maria Jankowiak <jankowiakmaria@gmail.com>
2015-03-01 14:52:00 +0000

    not working mess

--- app.js
+++ app.js
@@ -14,11 +14,36 @@ var crawl = function(error, parameters){
   }
 
   var providerConfig = config[parameters.provider];
+  var currentDay = parameters.startDate;
   var flightPage = webPage.create();
+
+  function proc(providerConfig, parameters, currentDay) {
+    processOneDay(flightPage, providerConfig, parameters, currentDay.format(providerConfig.dateFormat), function(error, data){
+      if(error){
+        console.log("----",'error', error);
+      }
+      else{
+        console.log("----",JSON.stringify(data, undefined, 4));
+      }
+      currentDay.add(1, "days");
+      if(currentDay <= parameters.endDate){
+        console.log("----", "next day", currentDay.format(providerConfig.dateFormat))
+        setTimeout(function(){
+          proc(providerConfig, parameters, currentDay);
+        }, 1000);
+      }else{
+        phantom.exit();
+      }
+    });
+  };
+
+  proc(providerConfig, parameters, currentDay);
+};
+
+var processOneDay = function(flightPage, providerConfig, parameters, currentDay, callback){
   var loadTimeout;
   var maxTimeout = 5000;
   var t;
-
 
   flightPage.onResourceReceived = function (response) {
     if(flightPage.url === providerConfig.resultPage){
@@ -27,8 +52,13 @@ var crawl = function(error, parameters){
     }
   };
 
+  flightPage.onConsoleMessage = function(msg) {
+      console.log('console: ' + msg);
+  };
+
+//todo: what if more than one flight in a day
   var getResults = function(flightPage, t) {
-    console.log("afterTimeout", Date.now() - t);
+    console.log(currentDay, "afterTimeout", Date.now() - t);
     var result = flightPage.evaluate(function(){
       var result = [];
       $("article.selectFlights:not(#businessPlusBannerOffer)").each(function(index, element){
@@ -43,46 +73,44 @@ var crawl = function(error, parameters){
       return result;
     });
 
-    console.log(JSON.stringify(result, undefined, 4));
-    phantom.exit();
+    flightPage.close(function(){
+      console.log("closed?!");
+    });
+    console.log(currentDay, JSON.stringify(result, undefined, 4));
+    callback(null, result);
   };
 
   flightPage.open(providerConfig.searchPage, function (status) {
     if(status === "success") {
-      //todo: wait until render?
-      ph.set(flightPage, ".stations select[title='Origin']", parameters.origin);
-      ph.set(flightPage, ".stations select[title='Destination']", parameters.destination);
-      ph.replace(flightPage, "[name='SearchInput$DeptDate']", parameters.startDate.format(providerConfig.dateFormat));
-      ph.replace(flightPage, "[name='SearchInput$RetDate']", parameters.endDate.format(providerConfig.dateFormat));
-  //$("#SearchInput_RoundTrip").click();
-  //$("#SearchInput_OneWay").click();
+      console.log(currentDay, 0);//.format(providerConfig.dateFormat));
+      setTimeout(function(){  //temporary fix
+      //todo: wait until loaded!!!!
+  console.log(currentDay, 1);
+        ph.set(flightPage, ".stations select[title='Origin']", parameters.origin);
+        ph.set(flightPage, ".stations select[title='Destination']", parameters.destination);
+        ph.replace(flightPage, "[name='SearchInput$DeptDate']", currentDay);//.format(providerConfig.dateFormat));
+        //ph.replace(flightPage, "[name='SearchInput$RetDate']", parameters.endDate.format(providerConfig.dateFormat));
+    //$("#SearchInput_RoundTrip").click();
+    //$("#SearchInput_OneWay").click();
+  console.log(currentDay, 2);
+        //todo: add the rest of parameters
 
-      //todo: add the rest of parameters
-
-      flightPage.evaluate(function() {
-        $("#SearchInput_ButtonSubmit").click(); //todo: also wait
-      });
-      t = Date.now();
-
-      loadTimeout = setTimeout(function(){  //wait until render
-        getResults(flightPage, t);
-      }, maxTimeout);
+        flightPage.evaluate(function() {
+          $("#SearchInput_OneWay").click();
+          $("#SearchInput_ButtonSubmit").click(); //todo: also wait
+        });
+        t = Date.now();
+  console.log(currentDay, 3);
+        loadTimeout = setTimeout(function(){  //wait until page is loaded
+          getResults(flightPage, t);
+        }, maxTimeout);
+      }, 0);
     }
     else{
-      phantom.exit();
+      //flightPage.close();
+      callback('Unable to load page!', null);
     }
   });
 };
 
 input.getParameters(crawl);
-
-
-
-
-
-/*var isReady = false;
-
-flightPage.onConsoleMessage = function(msg) {
-    console.log('console: ' + msg);
-};
-*/
