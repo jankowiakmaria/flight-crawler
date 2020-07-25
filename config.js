32db419a4f79d1f4bac7c38da94fa38d5419e673
Maria Jankowiak <jankowiakmaria@gmail.com>
2020-07-25 13:02:08 +0100

    Wait until page is loaded (#7)
    * Extracted selectors to config file (refactor)
    
    * checkpoint 1

--- config.js
+++ config.js
@@ -4,7 +4,14 @@ var config = {
   ryanair: {
     searchPage: "https://www.bookryanair.com/SkySales/Booking.aspx#Search",
     resultPage: "https://www.bookryanair.com/SkySales/Booking.aspx#Select",
-    dateFormat: "DD/MM/YYYY"
+    dateFormat: "DD/MM/YYYY",
+    selectors: {
+      origin: ".stations select[title='Origin']",
+      destination: ".stations select[title='Destination']",
+      departureDate: "[name='SearchInput$DeptDate']",
+      oneWay: "#SearchInput_OneWay",
+      search: "#SearchInput_ButtonSubmit"
+    }
   }
 };
 
